e23de143ad3323394764ee392064bde28d31c694
Tyler <tylersundquist3@gmail.com>
2021-10-28 19:32:16 -0600

    adjusted syntax

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -1,26 +1,22 @@
-const router = require('express').Router();
-const { Review } = require('../../models');
+const router = require("express").Router();
+const { Review } = require("../../models");
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {});
 
-});
-
-router.post('/', (req, res) => {
-    Review.create({
-        // will want to change to review_text once it's also changed in review.js
-        review_text: req.body.review_text,
-        user_id: req.body.user_id,
-        content_id: req.body.content_id
-    })
-    .then(the_powder_project_db => res.json(the_powder_project_db))
-    .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
+router.post("/", (req, res) => {
+  Review.create({
+    // will want to change to review_text once it's also changed in review.js
+    review_text: req.body.review_text,
+    user_id: req.body.user_id,
+    content_id: req.body.content_id,
+  })
+    .then((the_powder_project_db) => res.json(the_powder_project_db))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
-
-});
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
