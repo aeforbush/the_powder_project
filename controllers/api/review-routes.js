2e9e3d9751ca57a7f9e46e1849f6731e1547ac64
jazzmbarry <jazzmbarry@gmail.com>
2021-10-30 09:28:06 -0600

    merging fix

--- controllers/api/review-routes.js
+++ controllers/api/review-routes.js
@@ -12,9 +12,9 @@ router.get("/", (req, res) => {
     });
 });
 
-router.get("/id", (req, res) => {
+router.get("/:id", (req, res) => {
   Review.findOne({
-    where: {id:req.params.id},
+    where: { id: req.params.id },
     attributes: ["id", "review_text", "user_id", "content_id"],
   })
     .then((the_powder_project_db) => res.json(the_powder_project_db))
@@ -29,7 +29,7 @@ router.post("/", (req, res) => {
     // will want to change to review_text once it's also changed in review.js
     review_text: req.body.review_text,
     user_id: req.body.user_id,
-    content_id: req.body.content_id,
+    content_id: req.body.content_id
   })
     .then((the_powder_project_db) => res.json(the_powder_project_db))
     .catch((err) => {
@@ -40,13 +40,13 @@ router.post("/", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   Review.destroy({
-    where : {id:req.params.id}
+    where: { id: req.params.id }
   })
-  .then((the_powder_project_db) => res.json(the_powder_project_db))
-  .catch((err) => {
-    console.log(err);
-    res.status(400).json(err);
-  });
+    .then((the_powder_project_db) => res.json(the_powder_project_db))
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
