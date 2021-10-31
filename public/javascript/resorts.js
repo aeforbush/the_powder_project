760b1c1229c71e8407d5eb640a03a072d984f255
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-10-31 10:14:19 -0600

    debugged resort routes

--- public/javascript/resorts.js
+++ public/javascript/resorts.js
@@ -1,32 +1,32 @@
-// async function commentFormHandler(event) {
-//     event.preventDefault();
+async function commentFormHandler(event) {
+    event.preventDefault();
 
-//     const review_text = document.querySelector('input[name="comment-body"]').value.trim();
+    const review_text = document.querySelector('input[name="comment-body"]').value.trim();
 
-//     const post_id = window.location.toString().split('/')[
-//         window.location.toString().split('/').length - 1
-//     ];
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
+    ];
 
-//     if (review_text) {
-//         const response = await fetch('/api/resorts', {
-//             method: 'POST',
-//             body: JSON.stringify({
-//                 post_id,
-//                 review_text
-//             }),
-//             headers: {
-//                 'Content-Type': 'application/json'
-//             }
-//         });
+    if (review_text) {
+        const response = await fetch('/api/resorts', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                review_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
-//         if (response.ok) {
-//             document.location.reload();
+        if (response.ok) {
+            document.location.reload();
 
-//         } else {
-//             alert(response.statusText);
-//             document.querySelector('#comment-form').style.display = "block";
-//         }
-//     }
-// }
+        } else {
+            alert(response.statusText);
+            document.querySelector('#comment-form').style.display = "block";
+        }
+    }
+}
 
-// document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
