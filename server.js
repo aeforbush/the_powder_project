5983c61c714bfe83ad58513d04699bbed6e5b585
jazzmbarry <jazzmbarry@gmail.com>
2021-11-02 20:57:44 -0600

    updated stylesheet with background.png and more styles

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ const app = express();
 const PORT = process.env.PORT || 3005;
 
 // app middleware
+// app.use(express.static('images')); 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
