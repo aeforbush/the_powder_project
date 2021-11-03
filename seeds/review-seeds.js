556cad1156c8be31a6e918b15532bf80eeb32704
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-11-02 18:10:15 -0600

    removed resort_id as fk constraint

--- seeds/review-seeds.js
+++ seeds/review-seeds.js
@@ -3,19 +3,19 @@ const { Review } = require('../models');
 const reviewData = [{
         review_text: "Greatest snow on earth!",
         user_id: 1,
-        resort_id: 1
+        // resort_id: 1
         
     },
     {
         review_text: "Great family vacation spot!",
-        user_id: 2,
-        resort_id: 2
+        user_id: 1,
+        // resort_id: 2
        
     },
     {
         review_text: "One of Utah's hidden gems!",
-        user_id: 3,
-        resort_id: 3
+        user_id: 1,
+        // resort_id: 3
       
     }
 ];
