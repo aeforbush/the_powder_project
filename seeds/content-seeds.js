c82cda9616eec14648c29d1efe598d1131ce66a4
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-11-03 18:54:59 -0600

    corrected spelling

--- seeds/content-seeds.js
+++ seeds/content-seeds.js
@@ -4,7 +4,7 @@ const contentData = [
   {
     title: "The Powder Project Mission",
     content:
-      "A monster storm is in the futurecast and you have time on the books to slip away, grab first chair and blissfully cruise those first tracks in some fresh POW, but where?  At The Powder Project our goal is to help you find your next rippin powder day.  Our research provides winter sport enthusiasts with essential information about snow quality and depth at Uthas most highly rated Mountain Resorts.  Get your gear ready for an awsome adventure!",
+      "A monster storm is in the futurecast and you have time on the books to slip away, grab first chair and blissfully cruise those first tracks in some fresh POW, but where?  At The Powder Project our goal is to help you find your next rippin powder day.  Our research provides winter sport enthusiasts with essential information about snow quality and depth at Utah's most highly rated Mountain Resorts.  Get your gear ready for an awesome adventure!",
   },
 ];
 
