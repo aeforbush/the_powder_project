5f8f550cc0b73d2d7d689ae9bce3516a26697d58
aeforbush <87487655+aeforbush@users.noreply.github.com>
2021-11-07 09:18:46 -0700

    cleaned up commented out code

--- controllers/api/resort-routes.js
+++ controllers/api/resort-routes.js
@@ -15,10 +15,6 @@ router.get("/", (req, res) => {
           attributes: ["username"],
         },
       },
-      // {
-      //   model: User,
-      //   attributes: ["username"],
-      // },
     ],
   })
     .then((dbResortData) => {
